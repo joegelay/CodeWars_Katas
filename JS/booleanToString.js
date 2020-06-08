24dd5d653e2cb4128c8c2fb5fc420818cb0d3b58
joegelay <joegelay@gmail.com>
2020-06-08 14:13:08 -0600

    add booleantoString.js

---
+++ JS/booleanToString.js
@@ -0,0 +1,10 @@
+// INSTRUCTION
+
+// Implement a function which convert the given boolean value into its string representation.
+
+
+// SOLUTION
+
+function booleanToString(b){
+    return b ? "true" : "false"
+}
