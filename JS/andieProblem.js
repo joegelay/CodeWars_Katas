3c7650d3da72aa532701135882e7146a34cc98c4
joegelay <joegelay@gmail.com>
2020-06-23 21:56:10 -0600

    add andieProblem.js

---
+++ JS/andieProblem.js
@@ -0,0 +1,17 @@
+// INSTRUCTION
+
+// Implement a function that takes an array and some other arguments and then removes the other arguments from that array
+
+// SOLUTION
+
+function andieSolution(array,stuffToRemoveFromArray) {
+    
+    const argsArray = [...arguments]
+    const originalArray = argsArray.shift()
+   
+    const result = originalArray.filter(number => {
+        return !argsArray.includes(number)
+    })
+
+   return result 
+}
